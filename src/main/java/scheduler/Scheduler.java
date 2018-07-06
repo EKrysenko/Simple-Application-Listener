23914c368a2b856760d7db32add33eaf946814c1
Egor Vyrva <Egor_Vyrva@epam.com>
2018-07-06 12:28:27 +0300

    add scheduler transfer size char array
    refactor

--- src/main/java/scheduler/Scheduler.java
+++ src/main/java/scheduler/Scheduler.java
@@ -5,28 +5,37 @@ import java.io.*;
 public class Scheduler {
     private String path;
     private int command;
+    private int sizeCharArray;
 
-    public Scheduler(String path) {
+    public Scheduler(String path, int sizeCharArray) {
         this.path = path;
+        this.sizeCharArray = sizeCharArray;
     }
 
     public int getCommand() {
+
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            command = Integer.valueOf(br.readLine());
+            int message = Integer.parseInt(br.readLine());
+            command = message % 10;
+            sizeCharArray = message / 10;
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return command;
     }
 
-    public void setCommand(int command) {
+    public void sendMessage(int command) {
+
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-            bw.write(String.valueOf(command));
+            bw.write(String.valueOf(sizeCharArray * 10 + command));
             this.command = command;
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
+    public int getSizeCharArray() {
+        return sizeCharArray;
+    }
 }
