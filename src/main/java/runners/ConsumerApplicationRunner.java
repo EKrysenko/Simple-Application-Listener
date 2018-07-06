ba64be54fe8122f8bed68058d7fc651b332ab620
Egor Vyrva <Egor_Vyrva@epam.com>
2018-07-06 18:57:06 +0300

    fix TCP writer and reader

--- src/main/java/runners/ConsumerApplicationRunner.java
+++ src/main/java/runners/ConsumerApplicationRunner.java
@@ -2,79 +2,69 @@ package runners;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.CharBuffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-
-import static java.nio.channels.FileChannel.MapMode;
 
 public class ConsumerApplicationRunner {
 
-    private static final String PATH = "/dev/shm/image-cache";
-    private static final String MODE = "rw";
-    private static final String NAMED_PIPE = "/home/uliana/Documents/lgi/docker/FILE.in";
-    private static final int SIZE = 74883500;
     private static final String TCP_HOST = "localhost";
     private static final int TCP_CONSUMER_PORT = 8000;
     private static final int TCP_PRODUCER_PORT = 9000;
+    private final int BLOCK_SIZE = 60_000;
 
-    public void runCobolApp() throws FileNotFoundException {
+    public void runCobolApp() {
 
         executeApp();
 
     }
 
-    private void executeApp() throws FileNotFoundException {
+    private void executeApp() {
 
         String readData;
 
         try (ServerSocket serverSocket = new ServerSocket(TCP_CONSUMER_PORT)) {
 
-//            long start = System.nanoTime();
-
             Socket client = serverSocket.accept();
             try (DataInputStream dataInputStream = new DataInputStream(client.getInputStream())) {
-                readData = dataInputStream.readUTF();
+                readData = readTCP(dataInputStream);
             }
 
             try (Socket socket = new Socket(TCP_HOST, TCP_PRODUCER_PORT);
                  DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
-                dataOutputStream.writeUTF(readData);
-                dataOutputStream.flush();
+                writeTCP(readData, dataOutputStream);
             }
 
-//            long finish = System.nanoTime();
-
-//            System.out.println("elapsed time is " + (finish - start) / 1e6 + " ms");
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
+    private void writeTCP(String sendData, DataOutputStream dataOutputStream) throws IOException {
+        int stringLength = sendData.length();
+        int number = stringLength / BLOCK_SIZE;
+        dataOutputStream.writeInt(number);
+        String[] stringArr = new String[number];
 
-    private char[] readFromSHM(FileChannel channel) throws IOException {
-
-        MappedByteBuffer buffer = channel.map(MapMode.READ_WRITE, 0, SIZE);
-        CharBuffer charBuf = buffer.asCharBuffer();
-        char[] received = new char[SIZE/2];
-
-        charBuf.get(received);
-
-        return received;
+        for (int i = 0; i < number; i++) {
+            final int beginIndex = i * BLOCK_SIZE;
+            final int lastInd = beginIndex + BLOCK_SIZE;
+            if (lastInd > stringLength)
+                stringArr[i] = sendData.substring(beginIndex, stringLength);
+            else
+                stringArr[i] = sendData.substring(beginIndex, lastInd);
+            dataOutputStream.writeUTF(stringArr[i]);
+        }
+        dataOutputStream.flush();
     }
 
-    private void writeToSHM(FileChannel channel, char[] outputChars) throws Exception {
-
-
-        MappedByteBuffer buffer = channel.map(MapMode.READ_WRITE, 0, SIZE);
-        CharBuffer charBuffer = buffer.asCharBuffer();
-        charBuffer.clear();
-
-        charBuffer.put(outputChars);
+    private String readTCP(DataInputStream dataInputStream) throws IOException {
+        final int len = dataInputStream.readInt();
+        final StringBuilder sb = new StringBuilder(len * BLOCK_SIZE);
+        for (int i = 0; i < len; i++)
+            sb.append(dataInputStream.readUTF());
+        return sb.toString();
     }
+
 }
