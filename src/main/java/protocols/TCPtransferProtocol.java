68908a4817afcdf35f04a0db6f5422de2d78f1ef
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-07-06 17:17:41 +0300

    Initial

---
+++ src/main/java/protocols/TCPtransferProtocol.java
@@ -0,0 +1,11 @@
+package protocols;
+
+import interfaces.TransferProtocol;
+
+public class TCPtransferProtocol implements TransferProtocol {
+
+    @Override
+    public void execute() {
+
+    }
+}
