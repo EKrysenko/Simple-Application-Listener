f314004fc4268904fee65240d9de49597e01188a
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-07-19 19:28:49 +0300

    Remove SHM scheduler

--- src/main/java/protocols/SHMtransferProtocol.java
+++ src/main/java/protocols/SHMtransferProtocol.java
@@ -4,11 +4,12 @@ import dataCreater.DataCreator;
 
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
+import java.util.List;
 
-import static constants.Constants.SHARED_MEMORY_PATH;
-import static constants.Constants.SIZE;
+import static constants.Constants.*;
 import static java.nio.channels.FileChannel.MapMode.READ_WRITE;
 
 public class SHMtransferProtocol implements TransferProtocol {
@@ -17,37 +18,69 @@ public class SHMtransferProtocol implements TransferProtocol {
     private final int DATA_AREA_START = 64;
     private final int CONSUMER_OFFSET = 32;
     private final int PRODUCER_OFFSET = 0;
+    private MappedByteBuffer consumerUtilBuffer;
+    private MappedByteBuffer producerUtilBuffer;
+    private MappedByteBuffer producerDataBuffer;
+    private MappedByteBuffer consumerDataBuffer;
 
     @Override
-    public void executeProducer() {
+    public void executeProducer(int lowSizePackage, int highSizePackage, int transferTime) {
 
-        String data = DataCreator.createFixedSizePackage(100);
+        List<String> sendData = DataCreator.createRandomSizePackage(1000, lowSizePackage, highSizePackage);
 
         try (RandomAccessFile sharedMemory = new RandomAccessFile(SHARED_MEMORY_PATH, "rw");
              FileChannel channel = sharedMemory.getChannel()) {
 
             long start = System.nanoTime();
+            long countOfBytes = 0;
+            long countOfPackage = 0;
 
-            clearUtilArea(channel, CONSUMER_OFFSET);
+            producerUtilBuffer = channel.map(READ_WRITE, PRODUCER_OFFSET, 32);
+            producerDataBuffer = channel.map(READ_WRITE, DATA_AREA_START, SIZE / 2);
+            consumerUtilBuffer = channel.map(READ_WRITE, CONSUMER_OFFSET, 32);
+            consumerDataBuffer = channel.map(READ_WRITE, DATA_AREA_START + SIZE / 2, SIZE / 2);
 
-            writeData(data, channel, PRODUCER_OFFSET, DATA_AREA_START);
+            label:
+            while(true) {
 
-            int dataSize = 0;
-            FileLock checkConsumerUtil;
-            while (true) {
-                checkConsumerUtil = channel.lock(CONSUMER_OFFSET, 32, false);
-                if ((dataSize = channel.map(READ_WRITE, CONSUMER_OFFSET, SIZE).asIntBuffer().get()) != CLEAR_UTIL) {
-                    break;
+                for (String data : sendData) {
+
+                    clearUtilArea(channel, CONSUMER_OFFSET);
+
+                    writeData(data, channel, PRODUCER_OFFSET, DATA_AREA_START, consumerUtilBuffer, producerDataBuffer);
+
+                    int dataSize;
+                    FileLock checkConsumerUtil;
+                    while (true) {
+                        checkConsumerUtil = channel.lock(CONSUMER_OFFSET, 32, true);
+                        if ((dataSize = consumerUtilBuffer.asIntBuffer().get()) != CLEAR_UTIL) {
+                            break;
+                        }
+                        checkConsumerUtil.release();
+                    }
+
+                    String readData = readData(channel, dataSize, DATA_AREA_START + data.length());
+
+                    clearUtilArea(channel, checkConsumerUtil, CONSUMER_OFFSET);
+
+                    countOfPackage++;
+                    countOfBytes += readData.length();
+
+                    if (System.nanoTime() - start > (long) (transferTime * 1e09)) {
+                        break label;
+                    }
                 }
-                checkConsumerUtil.release();
             }
-            String inputData = readData(channel, dataSize, DATA_AREA_START + data.length());
-
-            clearUtilArea(channel, checkConsumerUtil, CONSUMER_OFFSET);
 
             long finish = System.nanoTime();
-            System.out.println(inputData.equals(data));
+
+            System.out.println("Test for packages from " + lowSizePackage + " to " + highSizePackage + " bytes:");
+            System.out.println("Number of packages\nNumber of Mb\nTime\n");
+
+            System.out.println(countOfPackage);
+            System.out.println(countOfBytes / 1024 / 1024);
             System.out.println((finish - start) / 1e6);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -59,11 +92,15 @@ public class SHMtransferProtocol implements TransferProtocol {
         try (RandomAccessFile sharedMemory = new RandomAccessFile(SHARED_MEMORY_PATH, "rw");
              FileChannel channel = sharedMemory.getChannel()) {
 
+            producerUtilBuffer = channel.map(READ_WRITE, PRODUCER_OFFSET, 32);
+            producerDataBuffer = channel.map(READ_WRITE, DATA_AREA_START, SIZE / 2);
+            consumerDataBuffer = channel.map(READ_WRITE, DATA_AREA_START + SIZE / 2, SIZE / 2);
+
             clearUtilArea(channel, PRODUCER_OFFSET);
 
             while (true) {
-                FileLock checkProducerUtil = channel.lock(PRODUCER_OFFSET, 32, false);
-                int message = channel.map(READ_WRITE, PRODUCER_OFFSET, SIZE).asIntBuffer().get();
+                FileLock checkProducerUtil = channel.lock(PRODUCER_OFFSET, 32, true);
+                int message = producerUtilBuffer.asIntBuffer().get();
                 if (message != CLEAR_UTIL) {
 
                     String inputData = readData(channel, message, DATA_AREA_START);
@@ -72,7 +109,7 @@ public class SHMtransferProtocol implements TransferProtocol {
 
                     String outputData = inputData;
 
-                    writeData(outputData, channel, CONSUMER_OFFSET, DATA_AREA_START + inputData.length());
+                    writeData(outputData, channel, CONSUMER_OFFSET, DATA_AREA_START + inputData.length(), consumerUtilBuffer, consumerDataBuffer);
                 }
                 checkProducerUtil.release();
             }
@@ -84,7 +121,7 @@ public class SHMtransferProtocol implements TransferProtocol {
     }
 
     private String readData(FileChannel channel, int dataSize, int startIndex) throws IOException {
-        FileLock dataAreaLock = channel.lock(startIndex, dataSize, false);
+        FileLock dataAreaLock = channel.lock(startIndex, dataSize, true);
         char[] chars = new char[dataSize];
         channel.map(READ_WRITE, startIndex, SIZE).asCharBuffer().get(chars);
         String data = new String(chars);
@@ -92,12 +129,12 @@ public class SHMtransferProtocol implements TransferProtocol {
         return data;
     }
 
-    private void writeData(String data, FileChannel channel, int utilAreaStart, int dataAreaStart) throws IOException {
-        FileLock utilAreaLock = channel.lock(utilAreaStart, 32, false);
-        channel.map(READ_WRITE, utilAreaStart, SIZE).asIntBuffer().put(data.length());
+    private void writeData(String data, FileChannel channel, int utilAreaStart, int dataAreaStart, MappedByteBuffer utilAreaBuffer, MappedByteBuffer dataAreaBuffer) throws IOException {
+        FileLock utilAreaLock = channel.lock(utilAreaStart, 32, true);
+        utilAreaBuffer.asIntBuffer().put(data.length());
 
-        FileLock dataAreaLock = channel.lock(dataAreaStart, data.length(), false);
-        channel.map(READ_WRITE, dataAreaStart, SIZE).asCharBuffer().put(data);
+        FileLock dataAreaLock = channel.lock(dataAreaStart, data.length(), true);
+        dataAreaBuffer.asCharBuffer().put(data);
         dataAreaLock.release();
         utilAreaLock.release();
     }
@@ -108,7 +145,7 @@ public class SHMtransferProtocol implements TransferProtocol {
     }
 
     private void clearUtilArea(FileChannel channel, int startIndex) throws IOException {
-        FileLock lock = channel.lock(startIndex, 32, false);
+        FileLock lock = channel.lock(startIndex, 32, true);
         clearUtilArea(channel, lock, startIndex);
     }
 
