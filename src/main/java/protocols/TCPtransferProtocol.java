0282e5e0b6c6d2bbe9046adce820b4469d6dfe3d
Egor Vyrva <Egor_Vyrva@epam.com>
2018-07-19 15:08:40 +0300

    refactor

--- src/main/java/protocols/TCPtransferProtocol.java
+++ src/main/java/protocols/TCPtransferProtocol.java
@@ -2,7 +2,9 @@ package protocols;
 
 import dataCreater.DataCreator;
 
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
@@ -17,7 +19,7 @@ public class TCPtransferProtocol implements TransferProtocol {
 
         List<String> sendData = null;
         try {
-            sendData = DataCreator.createRandomSizePackage(2 * 1024, LOW_BOUNDER_IN_BYTES, UP_BOUNDER_IN_BYTES);
+            sendData = DataCreator.createRandomSizePackage(200 * 1024, LOW_BOUNDER_IN_BYTES, UP_BOUNDER_IN_BYTES);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
@@ -31,25 +33,32 @@ public class TCPtransferProtocol implements TransferProtocol {
             Socket client = serverSocket.accept();
 
             long start = System.nanoTime();
-            long i = 0;
-            long j = 0;
+            long countOfBytes = 0;
+            long countOfPackage = 0;
             try (DataInputStream dataInputStream = new DataInputStream(client.getInputStream())) {
 
-                while (System.nanoTime() - start < (long) (TRANSFER_TIME_IN_SECONDS * 1e09)) {
+                label:
+                while (true) {
+
                     for (String onePackage : sendData) {
                         dataOutputStream.writeUTF(onePackage);
                         String respData = dataInputStream.readUTF();
-                        j++;
-                        i += respData.length();
+                        countOfPackage++;
+                        countOfBytes += respData.length();
+
+                        if (System.nanoTime() - start > (long) (TRANSFER_TIME_IN_SECONDS * 1e09)) {
+                            break label;
+                        }
                     }
+
                 }
                 dataOutputStream.writeUTF("-1");
             }
 
             long finish = System.nanoTime();
 
-            System.out.println("number of packages: " + j);
-            System.out.println("Total transfer data size in Mb: " + (i / 1024 / 1024));
+            System.out.println("number of packages: " + countOfPackage);
+            System.out.println("Total transfer data size in Mb: " + (countOfBytes / 1024 / 1024));
             System.out.println((finish - start) / 1e6);
 
         } catch (Exception e) {
@@ -78,47 +87,6 @@ public class TCPtransferProtocol implements TransferProtocol {
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void writeTCP(String sendData, DataOutputStream dataOutputStream) throws IOException {
-        int stringLength = sendData.length();
-        int number = stringLength / BLOCK_SIZE;
-        dataOutputStream.writeInt(number);
-        String[] stringArr = new String[number];
-
-        for (int i = 0; i < number; i++) {
-            final int beginIndex = i * BLOCK_SIZE;
-            final int lastInd = beginIndex + BLOCK_SIZE;
-            if (lastInd > stringLength)
-                stringArr[i] = sendData.substring(beginIndex, stringLength);
-            else
-                stringArr[i] = sendData.substring(beginIndex, lastInd);
-            dataOutputStream.writeUTF(stringArr[i]);
-        }
-        dataOutputStream.flush();
-    }
-
-    private String readTCP(Socket client) throws IOException {
-        final StringBuilder sb;
-        try (DataInputStream dataInputStream = new DataInputStream(client.getInputStream())) {
-            final int len = dataInputStream.readInt();
-            sb = new StringBuilder(len * BLOCK_SIZE);
-            for (int i = 0; i < len; i++)
-                sb.append(dataInputStream.readUTF());
-        }
-        return sb.toString();
-    }
-
-    private void writeToFile(char[] received) {
-
-        try (FileWriter writer = new FileWriter(RECEIVED_FILE)) {
-
-            writer.write(received);
-            writer.flush();
-
-        } catch (IOException e) {
             e.printStackTrace();
         }
     }
