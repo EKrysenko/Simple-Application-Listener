085ecc70bc4816cc27b05abbdfc55423ff2d159d
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-07-23 12:07:00 +0300

    delete unused methods

--- src/main/java/protocols/TransferProtocol.java
+++ src/main/java/protocols/TransferProtocol.java
@@ -1,10 +1,5 @@
 package protocols;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public interface TransferProtocol {
 
     default void execute(String executor, String lowSizePackage, String highSizePackage, String transferTimeInSeconds) {
@@ -23,33 +18,5 @@ public interface TransferProtocol {
     void executeProducer(int lowSizePackage, int highSizePackage, int transferTimeInSeconds);
 
     void executeConsumer();
-
-    default void writeToFile(String path, char[] received) {
-
-        try (FileWriter writer = new FileWriter(path)) {
-
-            writer.write(received);
-            writer.flush();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    default char[] readFile(String path) {
-
-        String line;
-        StringBuilder builder = new StringBuilder();
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            while ((line = br.readLine()) != null) {
-                builder.append(line).append("\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return builder.toString().toCharArray();
-    }
-
 
 }
