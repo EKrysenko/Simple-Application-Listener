a260744de8b38b468e278f793cb6b07171e2fc44
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-07-25 18:42:59 +0300

    Clear SHM. Prepare to merge 82

--- src/main/java/protocols/SHMtransferProtocol.java
+++ src/main/java/protocols/SHMtransferProtocol.java
@@ -1,71 +1,23 @@
 package protocols;
 
-import constants.Constants;
-import schedulers.NamedPipeScheduler;
-import schedulers.Scheduler;
-
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.CharBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 
-import static constants.Constants.*;
+import static constants.Constants.SIZE;
 
 public class SHMtransferProtocol implements TransferProtocol {
 
     @Override
     public void executeProducer(int lowSizePackage, int highSizePackage, int transferTime) {
 
-        char[] charBuffer = readFile(Constants.SEND_FILE);
-        Scheduler scheduler = new NamedPipeScheduler(NAMED_PIPE, charBuffer.length);
-
-        try (RandomAccessFile sharedMemory = new RandomAccessFile(SHARED_MEMORY_PATH, "rw");
-             FileChannel channel = sharedMemory.getChannel()) {
-
-            long start = System.nanoTime();
-
-            writeToSHM(channel, charBuffer);
-            scheduler.sendMessage(0);
-
-            if (scheduler.getCommand() == 1) {
-                charBuffer = readFromSHM(channel, scheduler.getSizeCharArray());
-            } else {
-                charBuffer = "no data received".toCharArray();
-            }
-
-            long finish = System.nanoTime();
-
-            writeToFile(RECEIVED_FILE, charBuffer);
-            System.out.println((finish - start) / 1e6);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void executeConsumer() {
 
-        NamedPipeScheduler scheduler = new NamedPipeScheduler(NAMED_PIPE);
-
-        if (scheduler.getCommand() == 0) {
-
-            try (RandomAccessFile sharedMemory = new RandomAccessFile(SHARED_MEMORY_PATH, "rw");
-                 FileChannel channel = sharedMemory.getChannel()) {
-
-                char[] inputChars = readFromSHM(channel, scheduler.getSizeCharArray());
-
-                // TODO: here we can add some logic to change input data before sending
-
-                writeToSHM(channel, inputChars);
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            scheduler.sendMessage(1);
-        }
     }
 
 
