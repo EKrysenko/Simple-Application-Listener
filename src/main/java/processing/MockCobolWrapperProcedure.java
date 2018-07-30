d4dae4467fd0966f64a8a277926160080c849a22
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-07-30 15:55:23 +0300

    add FJP parallelization add mock cobol invocation

---
+++ src/main/java/processing/MockCobolWrapperProcedure.java
@@ -0,0 +1,11 @@
+package processing;
+
+public class MockCobolWrapperProcedure {
+
+    public static void executeCobolProcedure(String[] data, int from, int to) {
+
+        for (int i = from; i < to; i++) {
+            data[i] = data[i].toUpperCase();
+        }
+    }
+}
