fdada2790c7f0cc6729d4f5aa906b620e43de5c4
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-07-30 15:12:00 +0300

    change structure as discribed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,33 @@
-import factories.SHMFactory;
-import factories.TCPFactory;
-import factories.TransferProtocolFactory;
-
-import static java.lang.Integer.parseInt;
+import servers.IPCServer;
+import servers.Server;
+import servers.TCPServer;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        if (args.length < 5 && "producer".equals(args[1])) {
+        if (args.length < 4) {
             System.out.println("Wrong input arguments.\n"
                     + "Specify also low size, high size for a package in bytes "
                     + "and transfer time in seconds");
             return;
         }
 
-        TransferProtocolFactory factory = getFactory(args[0]);
+        Server server;
 
-        if ("producer".equals(args[1])) {
-            factory.createProducer(
-                    parseInt(args[2]),
-                    parseInt(args[3]),
-                    parseInt(args[4]))
-                    .run();
-        }
-        if ("consumer".equals(args[1])) {
-            factory.createConsumer().run();
-        }
-        System.out.println("Wrong input arguments.\n"
-                + "Use producer/consumer for executor. \n" +
-                "by default consumer is created");
-
-    }
-
-    private static TransferProtocolFactory getFactory(String protocol) {
-        if ("SHM".equals(protocol)) {
-            return new SHMFactory();
-        }
-        if ("TCP".equals(protocol)) {
-            return new TCPFactory();
+        if ("IPC".equals(args[0])) {
+            server = IPCServer.getIPCServer();
+        } else if ("TCP".equals(args[0])) {
+            server = TCPServer.getTCPServer();
         } else {
             System.out.println("Wrong input arguments.\n"
-                    + "Use TCP/SHM.\n"
+                    + "Use TCP/IPC.\n"
                     + "By default TCP is started");
-            return new TCPFactory();
+            server = TCPServer.getTCPServer();
         }
+
+        server.run();
     }
+
 
 }
