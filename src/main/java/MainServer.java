a1fe2bf5785551b0778cdb2f1daca1676f9ad963
evgenii_krysenko <evgenii_krysenko@epam.com>
2018-08-02 18:08:55 +0300

    add cobol jni invocation

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -6,7 +6,9 @@ import server.processing.CallingCobol;
 public class MainServer {
 
     public static void main(String[] args) {
+
         System.out.println(new CallingCobol().adaptor("saaaaaaasd"));
+
         if (args.length < 4) {
             System.out.println("Wrong input arguments.\n"
                     + "Specify also low size, high size for a package in bytes "
